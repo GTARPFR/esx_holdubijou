19ebe8648028f86ea4f2d726b7739496208e0a36
xPiston <dydy.lavigne@hotmail.fr>
2018-04-20 10:59:35 +0200

    initial
    esx_holdup_bijou

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+Config.Locale = 'fr'
+Config.NumberOfCopsRequired = 0
+
+Stores = {
+	["Vangelico"] = {
+		position = { ['x'] = -619.79315185547, ['y'] = -229.25184631348, ['z'] = 38.057048797607 },
+		reward = math.random(10000,50000),
+		nameofstore = "Vangelico",
+		lastrobbed = 0
+	}
+}
